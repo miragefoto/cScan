414ecba740a41e8d547612e5b7ccff3ee719aeff
R. Eric Phillips <eric.phillips@thunderbolttech.net>
2018-09-26 19:53:25 -0400

    test

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+// Add BarcodeScanner
+import { BarcodeScanner } from '@ionic-native/barcode-scanner';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,20 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+    // store the scanned result
+    num: string;
+
+  // DI barcodeScanner
+  constructor(public navCtrl: NavController, 
+    private barcodeScanner: BarcodeScanner) {
 
   }
 
+  // new scan method
+  scan() {
+    this.barcodeScanner.scan().then(data => {
+        // this is called when a barcode is found
+        this.num = data.text
+  });      
+  }
 }
